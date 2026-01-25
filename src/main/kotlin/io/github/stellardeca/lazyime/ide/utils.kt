cb1ed4207cba44e5f4c620056dce7b3f0b1b6202
StellarDeca <166920291+StellarDeca@users.noreply.github.com>
2026-01-25 18:59:51 +0800

    Add utils.kt

---
+++ src/main/kotlin/io/github/stellardeca/lazyime/ide/utils.kt
@@ -0,0 +1,69 @@
+package io.github.stellardeca.lazyime.ide
+
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.notification.NotificationGroupManager
+import com.intellij.notification.NotificationType
+import io.github.stellardeca.lazyime.core.lib.*
+
+private const val NOTIFICATION_GROUP_ID = "lazyime.jetbrains"
+
+/// 获取编辑的文件类型 不在编辑窗口中时为null
+fun getLanguage(project: Project, document: Document): String {
+    val psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document) ?: return ""
+    return psiFile.language.id
+}
+
+/// 获取编辑器 光标 不支持多光标
+fun getCursor(editor: Editor): Cursor {
+    val document = editor.document
+    val pos = editor.caretModel.primaryCaret.logicalPosition
+
+    val row = pos.line
+    val charColumn = pos.column
+
+    // 获取当前行的 字符起始偏移
+    // 计算光标在本行内的字符跨度
+    val lineStartOffset = document.getLineStartOffset(row)
+    val lineEndOffset = document.getLineEndOffset(row)
+    val lengthInLine = charColumn.coerceAtMost(lineEndOffset - lineStartOffset)
+
+    // 获取该行光标前的字符串
+    val linePrefix = document.getText(
+        TextRange(
+            lineStartOffset,
+            lineStartOffset + lengthInLine
+        )
+    )
+
+    // 转为 UTF-8 字节数组并获取大小
+    val byteColumn = linePrefix.toByteArray(Charsets.UTF_8).size
+    return Cursor(row, byteColumn)
+}
+
+/// Info 弹窗 通知
+fun notifyInfo(message: String) {
+    NotificationGroupManager.getInstance()
+        .getNotificationGroup(NOTIFICATION_GROUP_ID)
+        .createNotification(message, NotificationType.INFORMATION)
+        .notify(null)
+}
+
+/// Warn 弹窗 通知
+fun notifyWarning(message: String) {
+    NotificationGroupManager.getInstance()
+        .getNotificationGroup(NOTIFICATION_GROUP_ID)
+        .createNotification(message, NotificationType.WARNING)
+        .notify(null)
+}
+
+/// Error 弹窗 通知
+fun notifyError(message: String) {
+    NotificationGroupManager.getInstance()
+        .getNotificationGroup(NOTIFICATION_GROUP_ID)
+        .createNotification(message, NotificationType.ERROR)
+        .notify(null)
+}
